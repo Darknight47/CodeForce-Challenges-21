011834d9aeec1a48f936c71b6c4970423baf307a
Darknight47 <farhadgunners@gmail.com>
2026-05-19 19:24:54 +0200

    math

---
+++ Garland.py
@@ -0,0 +1,47 @@
+"""
+
+--------------------------------------------- Link for the challenge: https://codeforces.com/problemset/problem/1279/A ---------------------------------------------
+
+Polycarp is sad — New Year is coming in few days but there is still no snow in his city. To bring himself New Year mood, he decided to decorate his house with some garlands.
+
+The local store introduced a new service this year, called "Build your own garland". 
+So you can buy some red, green and blue lamps, provide them and the store workers will solder a single garland of them. The resulting garland will have all the lamps you provided put in a line. 
+Moreover, no pair of lamps of the same color will be adjacent to each other in this garland!
+
+For example, if you provide 3 red, 3 green and 3 blue lamps, the resulting garland can look like this: 
+"RGBRBGBGR" ("RGB" being the red, green and blue color, respectively). Note that it's ok to have lamps of the same color on the ends of the garland.
+
+However, if you provide, say, 1 red, 10 green and 2 blue lamps then the store workers won't be able to build any garland of them. 
+Any garland consisting of these lamps will have at least one pair of lamps of the same color adjacent to each other. Note that the store workers should use all the lamps you provided.
+
+So Polycarp has bought some sets of lamps and now he wants to know if the store workers can build a garland from each of them.
+
+Input
+The first line contains a single integer t (1 ≤ t ≤ 100) — the number of sets of lamps Polycarp has bought.
+
+Each of the next t lines contains three integers r, g and b (1 ≤ r, g, b ≤ 10^9) — the number of red, green and blue lamps in the set, respectively.
+
+Output
+Print t lines — for each set of lamps print "Yes" if the store workers can build a garland from them and "No" otherwise.
+
+Input:
+3
+3 3 3
+1 10 2
+2 1 1
+
+Output:
+Yes
+No
+Yes
+"""
+cases = int(input())
+for _ in range(cases):
+    a, b, c = map(int, input().split())
+    mx = max(a, b, c)
+    rest = a + b + c - mx
+    
+    if mx <= rest + 1:
+        print("Yes")
+    else:
+        print("No")
